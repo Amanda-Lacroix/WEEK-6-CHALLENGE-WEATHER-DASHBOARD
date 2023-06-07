995b791131572cf0f9f81a08a3a2370519025a03
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-07 12:01:09 -0300

    revised index.html to add id's and revised  script.js to get html elements and add event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
-var cityName = document.querySelector(".searchInput");
-cityName.textContent;
+// Getting the required HTML elements
+var cityName = document.querySelector(".searchInput"). value;
+var searchButton = document.querySelector("#search-btn");
 
-function getWeather () {
+// Adding the event listener
+searchButton.addEventListener("click", function() {
+    var cityName = cityInput.value;
+    getWeather(cityName);
+  });
+
+function getWeather (cityName) {
 var requestApi = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&units=metric&appid=944e60e41c0c992134e31794af4ab0aa"
 
 fetch(requestApi)
@@ -10,7 +17,10 @@ fetch(requestApi)
 })
 .then(function (data) {
     console.log (data)
-});
+})
+.catch(function(error) {
+    console.log("Error:", error);
+  });
 }
 
 getWeather();
